2ddb8a7c6b6c508335a744b3071fac4ad2761d82
Reddyveeraballi19 <veeraballireddaiah889@gmail.com>
2025-12-26 14:16:31 +0530

    Add files via upload

---
+++ multiple of 5 usig if condition.py
@@ -0,0 +1,38 @@
+#multiples of 5 
+'''
+x=int(input())
+if x%5==0:
+    print("yes")
+else:
+    print("no")
+'''
+
+#even or odd
+'''
+x=int(input())
+if x%2==0:
+    print("even")
+elif x%5==0:
+    print("yes")
+else:
+    print("no")
+'''
+
+#loop basic
+'''
+x=[5,2,7,8,6,9]
+for i in x:
+    if i%2==0:
+        print(i)
+'''
+
+
+
+
+
+
+
+
+
+
+
